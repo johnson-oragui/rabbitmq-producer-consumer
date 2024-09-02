c8078b3a3fa6d7cfdb764d7da18b28be42c6066e
johnson-oragui <johnson-oragui@gmail.com>
2024-09-02 21:11:18 +0100

    feat: added variables for mail username, password, server, and port

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,10 @@ from decouple import config
 
 class Settings:
     DB_URL = str(config('DB_URL'))
+    MAIL_USERNAME = str(config('MAIL_USERNAME'))
+    MAIL_PASSWORD = str(config('MAIL_PASSWORD'))
+    MAIL_SERVER = str(config('MAIL_SERVER'))
+    MAIL_PORT = str(config('MAIL_PORT'))
 
 
 settings = Settings()
