ce368987ae2d2b33e6697fb08c752c9dfd408630
johnson-oragui <johnson-oragui@gmail.com>
2024-09-03 21:05:21 +0100

    feat: added heartbeat connection timeout

--- base/rabbitmq_conn.py
+++ base/rabbitmq_conn.py
@@ -6,7 +6,8 @@ import pika.exceptions
 @contextmanager
 def connect_pika():
     connection_params = pika.ConnectionParameters(
-        host='localhost'
+        host='localhost',
+        heartbeat=60,  # Set the heartbeat interval to 60 seconds
     )
     connection = pika.BlockingConnection(
         connection_params
