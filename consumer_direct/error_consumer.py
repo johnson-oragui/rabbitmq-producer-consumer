28fb65b1be5984d5095ecffed823df52326e46bf
johnson-oragui <johnson-oragui@gmail.com>
2024-09-04 16:39:55 +0100

    feat: completed error write functionality

--- consumer_direct/error_consumer.py
+++ consumer_direct/error_consumer.py
@@ -1,3 +1,16 @@
+import logging
+from config.logging_config import setup_logging
+
+# Setup logging
+setup_logging()
+
+# Get specific loggers
+error_logger = logging.getLogger('error_logger')
 
 def error_consumer(body):
-    pass
+    """Logs error messages to a specific error log file."""
+    try:
+        error_logger.error(f"Processing ERROR log: {body}")
+        print(f"Error logged: {body}")
+    except Exception as exc:
+        print(f"Failed to log error: {exc}")
