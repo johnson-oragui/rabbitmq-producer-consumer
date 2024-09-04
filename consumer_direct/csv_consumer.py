1c3bd4958345ccb6b11f63ed6816166bf24fab79
johnson-oragui <johnson-oragui@gmail.com>
2024-09-04 16:39:09 +0100

    feat: completed csv write functionality

--- consumer_direct/csv_consumer.py
+++ consumer_direct/csv_consumer.py
@@ -1,4 +1,26 @@
+import csv
+from uuid import uuid4
 
-
-def csv_consumer(body):
-    pass
+def csv_consumer(body: str):
+    """Processes CSV data by saving it to a .csv file.
+    
+    Args:
+        body (str): The content to be processed.
+    """
+    
+    print(f"Processing CSV data: {body}")
+    message = body.split()
+    file_name = f'csv_{str(uuid4())}.csv'
+    try:
+        with open(file_name, encoding='utf-8', mode='w', newline='') as csv_file:
+            writer = csv.writer(
+                csv_file,
+                delimiter=',',
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL
+            )
+            writer.writerow(message)
+        print(f'csv file saved as {file_name}')
+    except Exception as exc:
+        print(f'error in exporting csv: {exc}')
+        raise Exception from exc
